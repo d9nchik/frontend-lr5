2b2cc27829005c8e8d661a53671f173ccc36a243
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-04-09 17:52:39 +0300

    add oop style

--- index.js
+++ index.js
@@ -83,3 +83,129 @@ console.log(
     .map(({ name }) => name)
     .reduceRight((previous, current) => `${previous} ${current}`)
 );
+
+/* Object Oriented Programming */
+
+// Prototypes
+function Car(model) {
+  this.model = model;
+}
+
+Car.prototype.ridding = function () {
+  console.log(`ridding!`);
+};
+
+Car.prototype.getModel = function () {
+  console.log(`Car model is ${this.model}!`);
+};
+
+const PrivateCar = function (model, ownerName) {
+  Car.call(this, model);
+  this.ownerName = ownerName;
+  this._privateRideNoise = 'broom broom!';
+};
+
+PrivateCar.prototype = Object.create(Car.prototype);
+PrivateCar.prototype.constructor = PrivateCar;
+PrivateCar.prototype.getModel = function () {
+  console.log(`${this.model}: owner - ${this.ownerName}!`);
+  this.ridding();
+};
+PrivateCar.prototype.ridding = function () {
+  console.log(this._privateRideNoise);
+};
+
+const myCar1 = new Car('Mustang');
+myCar1.ridding();
+myCar1.getModel();
+
+const myPrivateCar = new PrivateCar('Porsche', 'Danylo');
+myPrivateCar.ridding();
+myPrivateCar.getModel();
+
+console.log(myPrivateCar instanceof Car);
+console.log(myPrivateCar instanceof PrivateCar);
+console.log(myCar1 instanceof Car);
+console.log(myCar1 instanceof PrivateCar);
+
+// Functional style, closure
+
+function funcCar(brand) {
+  this.brand = brand;
+
+  this.drive = function () {
+    console.log('driving!');
+  };
+
+  this.presentCar = function () {
+    console.log(`Car brand is ${this.brand}!`);
+  };
+}
+
+function funcPrivateCar(brand, ownerName) {
+  funcCar.call(this, brand);
+  this.ownerName = ownerName;
+  const _privateVoice = 'broom, broom!';
+
+  function privateDrive() {
+    console.log(_privateVoice);
+  }
+
+  this.presentCar = function () {
+    console.log(`${this.brand}: owner - ${this.ownerName}!`);
+    privateDrive();
+  };
+}
+
+const myCar2 = new funcCar('Toyota');
+myCar2.drive();
+myCar2.presentCar();
+
+const myPrivateCar2 = new funcPrivateCar('Mazda', 'Andrew');
+myPrivateCar2.drive();
+myPrivateCar2.presentCar();
+
+// ES6+
+class PerfectCar {
+  constructor(brand) {
+    this.brand = brand;
+  }
+
+  drive() {
+    console.log('zhoom zhoom!');
+  }
+
+  present() {
+    console.log(`Car name is ${this.brand}`);
+  }
+}
+
+class PerfectPrivateCar extends PerfectCar {
+  constructor(brand, ownerName) {
+    super(brand);
+    this.ownerName = ownerName;
+    this.#privateVoice = 'dzin dzin!';
+  }
+
+  #privateVoice;
+
+  #secretDrive() {
+    return this.#privateVoice;
+  }
+
+  present() {
+    console.log(
+      `Hi, I'm smart car ${this.brand}, and I love my owner ${
+        this.ownerName
+      }! ${this.#secretDrive()}!`
+    );
+  }
+}
+
+const myAnimal3 = new PerfectCar('BMW');
+myAnimal3.drive();
+myAnimal3.present();
+
+const myDog3 = new PerfectPrivateCar('RolsRoyce', 'Niemand');
+myDog3.drive();
+myDog3.present();
